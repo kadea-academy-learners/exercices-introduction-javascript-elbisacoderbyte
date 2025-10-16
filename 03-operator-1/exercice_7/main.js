b2b201a8d1ba4b04031f218a52a599f16dd98d4a
elbisacoderbyte <lengaeldad@gmail.com>
2025-10-16 21:29:08 +0100

    feat:operatorexercices 7

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -1,2 +1,13 @@
 // Exercice 7
 // Écrivez votre code ici
+let ageEnfant = 10;
+let agePère = ageEnfant * 2;
+let ageMère = agePère - 5;
+let ageGrandPère = (ageMère * 2) + (ageEnfant / 2);
+let ageOncle = agePère + 10;
+
+console.log("Âge de l'enfant:", ageEnfant);
+console.log("Âge du père:", agePère);
+console.log("Âge de la mère:", ageMère);
+console.log("Âge du grand-père:", ageGrandPère);
+console.log("Âge de l'oncle:", ageOncle);
