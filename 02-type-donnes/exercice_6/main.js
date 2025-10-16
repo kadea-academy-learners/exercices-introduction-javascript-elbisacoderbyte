34dafa3a862772fdf6b9e1f13ec8f4211e619e24
elbisacoderbyte <lengaeldad@gmail.com>
2025-10-16 21:18:47 +0100

    feat: solution exercice 6 types de donnÃ©es

--- 02-type-donnes/exercice_6/main.js
+++ 02-type-donnes/exercice_6/main.js
@@ -1,2 +1,7 @@
-// Exercice 6
-// Écrivez votre code ici
+let age = 25;
+let nom = "Randy";
+let estConnecte = true;
+let valeurNulle = null;
+let inconnue;
+
+module.exports = { age, nom, estConnecte, valeurNulle, inconnue };
