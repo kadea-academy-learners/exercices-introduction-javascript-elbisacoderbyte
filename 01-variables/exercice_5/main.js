eb18acc7bef2239698388090441f00d35622d8ad
elbisacoderbyte <lengaeldad@gmail.com>
2025-10-16 21:18:35 +0100

    feat: solutions exercices 4 et 5

--- 01-variables/exercice_5/main.js
+++ 01-variables/exercice_5/main.js
@@ -1,2 +1,9 @@
-// Exercice 5
-// Écrivez votre code ici
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+let celsius = 25;
+let fahrenheit = celsiusToFahrenheit(celsius);
+console.log("Température en Fahrenheit :", fahrenheit);
+
+module.exports = { celsiusToFahrenheit };
